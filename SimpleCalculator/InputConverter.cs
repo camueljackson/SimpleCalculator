af82d98cac70c1275c350bca5cf8db44ff338b89
camueljackson <camille.a.pelletier@gmail.com>
2019-06-26 21:43:33 -0400

    initial code for simple calculator; able to do basic operations

---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumeric(string argTextInput)
+        {
+            double convertedNumber;
+            if (!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expected a numeric value");
+            return convertedNumber;
+        }
+    }
+}
